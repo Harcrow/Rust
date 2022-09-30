3996485170c9fe7ed0872e43bb05fbe060670d1a
TJ <tyler.jowaisas@gmail.com>
2022-09-29 21:42:18 -0400

    moved away from CSV read crate.  Going to iterate over a read file and parse with commas

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,84 +1,78 @@
+extern crate csv;
 #[allow(unused)]
 #[allow(unused_imports)]
 extern crate nalgebra as na;
-extern crate csv;
 
 //use std::env;
-use std::io;
-//use  std::fs;
+use std::fs;
 //use  std::process;
 
+// The number of items divided by the number of rows equals the
+// number of columns.
 fn main() {
-    use na::Matrix3;
-    use na::Complex;
-//    use na::linalg::SVD;
+   // use na::Complex;
+   // use na::Matrix3;
 
-//I think I need to add/create some values to initially fill this?s
-    let mut m1 = Matrix3::zeros();
-    let mut x = 0;
-    let mut y = 0;
+    //use na::linalg::SVD;
 
+    //let mut m1 = Matrix3::repeat(0);
+
+    //let mut x = 0;
+    //let mut y = 0;
+    //let mut com = Complex { re: 0, im: 0 };
     //let args: Vec<String> = env::args().collect();
-
+    let path = "/home/tyler/Documents/Rust/matrix/src/matrix.csv";
     //let file_path = &args[1];
+    let csv_read = fs::read_to_string(path);
+    let _csv_iter = csv_read.iter();
+    /*    let record = result.expect("a CSV record");
+            let one = record[0].parse::<i64>().unwrap();
+            let two = record[1].parse::<i64>().unwrap();
 
-    
-    let mut rdr = csv::ReaderBuilder::new()
-	.has_headers(false)
-	.from_reader(io::stdin());
-    // Loop over each record.
-     for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-         // We will make this more friendly lat
+            com.re = one;
+            com.im = two;
 
-	 let record = result.expect("a CSV record");
-	 let mut com = Complex{re:record[0], im: record[1]};
-	  m1[(x,y)] =com;
-	 x += 1;
-	 if x >= 3 {
-	   x = 0;
-	    y +=1;
-	}
-	
-        // Print a debug version of the record.
-       // println!("{:?}", m1);
-	 }
-
-	 println!("{:4?}", m1);
-
-    /* 
-    let xx = Complex {
-	
-	re:698.680727128154,
-	im: -119.466079161084
-    };
-    let xy = Complex {
-	re:319.715801752267,
-	im:-96.7795868639316
-    };
-    let xz = Complex {
-	re:-148.105278843825,
-	    im:26.9792488607091
-    };
-    let yy = Complex {
-	re:631.430248485933,
-	im:-91.1754756430371
-    };
-    let yz = Complex {
-	re:-58.9247820986085,
-	im:27.8600860073641
-    };
-    let zz = Complex {
-	re:278.889024385914,
-	im: -21.3584451958786
-    };
-	*/
- //   let m1 = Matrix3::new(xx, xy, xz,
-   //                       xy, yy, yz,
- //                         xz, yz, zz );
-   
-   // let svd1 = SVD::new(m1, true, true);
-   // let _svd2 = m1.svd(true, true);
-   // println!("{:.4?}" ,svd1);
-    
+            m1[(x, y)] = com;
+            x += 1;
+            if x >= 3 {
+                x = 0;
+                y += 1;
+            }
+    */
+    // Print a debug version of the record.
+    println!("{:?}", csv_read);
 }
+/*
+let xx = Complex {
+
+re:698.680727128154,
+im: -119.466079161084
+};
+let xy = Complex {
+re:319.715801752267,
+im:-96.7795868639316
+};
+let xz = Complex {
+re:-148.105278843825,
+    im:26.9792488607091
+};
+let yy = Complex {
+re:631.430248485933,
+im:-91.1754756430371
+};
+let yz = Complex {
+re:-58.9247820986085,
+im:27.8600860073641
+};
+let zz = Complex {
+re:278.889024385914,
+im: -21.3584451958786
+};
+*/
+//   let m1 = Matrix3::new(xx, xy, xz,
+//                       xy, yy, yz,
+//                         xz, yz, zz );
+
+// let svd1 = SVD::new(m1, true, true);
+// let _svd2 = m1.svd(true, true);
+// println!("{:.4?}" ,svd1);
---
+++ matrix/target/debug/deps/matrix-54cad8c05ee1071b.d
@@ -0,0 +1,5 @@
+/home/tyler/Documents/Rust/matrix/target/debug/deps/matrix-54cad8c05ee1071b: src/main.rs
+
+/home/tyler/Documents/Rust/matrix/target/debug/deps/matrix-54cad8c05ee1071b.d: src/main.rs
+
+src/main.rs:
