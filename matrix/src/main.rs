9decdf9f7607f2084d91538308fb1b008f9eec80
TJ <tyler.jowaisas@gmail.com>
2022-11-07 22:31:49 -0500

    same commit but adding anything new? idk

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,5 +1,5 @@
 /* ultimate goal:
-
+x
 Well here's something that would be cool
 (but a not small project), digest some tables of retrieved objects
 (with polarizability values) and do a best fit of an ellipsoid model
@@ -17,16 +17,30 @@ SVD(conj_transpose(alpha) x alpha) <-- apparently what we do?
 
 use std::env;
 
+mod ellipsoid_polarizability;
 mod tensor_parse;
+
+use ellipsoid_polarizability as ell;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let epsilon = 0.001;
+
     let path = &args[1];
+
     let vec = tensor_parse::get_file(path.to_string());
     let mat = tensor_parse::complex_matrix(vec);
 
     let svd = mat.svd(true, true);
 
-    println!("{:?}", svd);
+    let ellipse = ell::ellipse(1.0, 2.0, 3.0, 4.0, 1.1);
+
+    
+    
+    println!("Right-Singular Vector {:.3?}", svd.v_t);
+    println!("Left-Singular Vector {:.3?}", svd.u);
+    println!("Singular Values {:.3?}", svd.singular_values);
+    println!("{:?}", ellipse);
+    
 }
