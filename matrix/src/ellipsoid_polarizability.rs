a453d60b552c2c1e7b0bcae2354fcd16eedcacd6
TJ <tyler.jowaisas@gmail.com>
2022-11-10 22:45:16 -0500

    have a model that does brute force best fit

--- matrix/src/ellipsoid_polarizability.rs
+++ matrix/src/ellipsoid_polarizability.rs
@@ -2,9 +2,11 @@
 use std::f32::consts::PI;
 
 pub fn ellipse(a1: f32, a2: f32, a3: f32, mu: f32) -> (f32, f32, f32) {
-   
     let volume = (4.0 / 3.0) * PI * a1 * a2 * a3;
-	
+    let normal = volume.cbrt();
+    let a1 = a1 / normal;
+    let a2 = a2 / normal;
+    let a3 = a3 / normal;
     //let normalize = (volume.cbrt()).powf(2.0);
 
     //let semi_axis = vec![a1, a2, a3];
@@ -18,44 +20,35 @@ pub fn ellipse(a1: f32, a2: f32, a3: f32, mu: f32) -> (f32, f32, f32) {
     println!("a2  to: {:.3?}", a2);
     println!("a3  to: {:.3?}", a3);
     */
-    
 
-    let ds = 0.5;
+    let ds = 1.0;
     let mut n1 = 0.0;
     let mut n2 = 0.0;
     let mut n3 = 0.0;
 
-    for x in 1..200000 {
+    for x in 1..10 {
         let s = x as f32 * ds;
-	//println!("S {:.10?}", n1*1000000000000000.0 );
-	
-        /*
 
-        matlab example
-
-        N(1,:) = aa1.*aa2.*aa3/2 .* sum(ds./((s+aa1.^2).*sqrt((s+aa1.^2).*(s+aa2.^2).*(s+aa3.^2))));
-        N(2,:) = aa1.*aa2.*aa3/2 .* sum(ds./((s+aa2.^2).*sqrt((s+aa1.^2).*(s+aa2.^2).*(s+aa3.^2))));
-        N(3,:) = aa1.*aa2.*aa3/2 .* sum(ds./((s+aa3.^2).*sqrt((s+aa1.^2).*(s+aa2.^2).*(s+aa3.^2))));
-
-        */
-
-        n1 = n1 + ds / (s + a1.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
-        n2 = n2 + ds / (s + a2.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
-        n3 = n3 + ds / (s + a3.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
+        n1 = n1
+            + ds / (s + a1.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
+        n2 = n2
+            + ds / (s + a2.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
+        n3 = n3
+            + ds / (s + a3.exp2() * ((s + a1.exp2()) * (s + a2.exp2()) * (s + a3.exp2())).sqrt());
     }
     n1 += n1 * (a1 * a2 * a3 / 2.0);
     n2 += n2 * (a1 * a2 * a3 / 2.0);
     n3 += n3 * (a1 * a2 * a3 / 2.0);
-    
+
     //println!("n1  to: {:.3?}", n1*10000000.00);
     //println!("n2  to: {:.3?}", n2);
     //println!("n3  to: {:.3?}", n3);
-    
+
     // println!("Volume is: {:.3?}", volume);
-    
-    let alpha1: f32 = volume * ((mu - 1.0) / (1.0 + (mu - 1.0) * n1));
-    let alpha2: f32 = volume * ((mu - 1.0) / (1.0 + (mu - 1.0) * n2));
-    let alpha3: f32 = volume * ((mu - 1.0) / (1.0 + (mu - 1.0) * n3));
+
+    let alpha1: f32 = volume * (mu - 1.0) / (1.0 + (mu - 1.0) * n1);
+    let alpha2: f32 = volume * (mu - 1.0) / (1.0 + (mu - 1.0) * n2);
+    let alpha3: f32 = volume * (mu - 1.0) / (1.0 + (mu - 1.0) * n3);
 
     return (alpha1, alpha2, alpha3);
 }
--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,5 +1,5 @@
 /* ultimate goal:
-x
+
 Well here's something that would be cool
 (but a not small project), digest some tables of retrieved objects
 (with polarizability values) and do a best fit of an ellipsoid model
@@ -16,31 +16,111 @@ SVD(conj_transpose(alpha) x alpha) <-- apparently what we do?
 */
 
 use std::env;
+use std::fs::File;
+use std::io::{Error, Write};
 
 mod ellipsoid_polarizability;
 mod tensor_parse;
 
 use ellipsoid_polarizability as ell;
 
-fn main() {
+
+fn main() -> Result<(), Error> {
+
+    let ELLIPSE_ITER = 100;
+    let ELLIPSE_RESOLUTION = 0.05;
+    
     let args: Vec<String> = env::args().collect();
-
-    let epsilon = 0.001;
-
     let path = &args[1];
+
+    let mut ell_vec = vec![0.0; ELLIPSE_ITER];
+    let perm_vec = vec![50, 1000, 10000, 50000, 200000];
+
+    let mut tensor_a1 = Vec::new();
+    let mut tensor_a2 = Vec::new();
+    let mut tensor_a3 = Vec::new();
+
+    let mut index = 10.0;
+    let mut count = 0;
+
+    //let mut file = File::create("ellipse.csv")?;
+
+    //Generates the vector that will be used to generate 1000^4 ellipse tensor values
+    while ell_vec[ELLIPSE_ITER - 1] == 0.0 {
+        ell_vec[count] = index + ELLIPSE_RESOLUTION;
+        index += ELLIPSE_RESOLUTION;
+        count += 1;
+    }
+    println!("ell_vec {:?}", ell_vec);
+    let _epsilon = 0.001;
 
     let vec = tensor_parse::get_file(path.to_string());
     let mat = tensor_parse::complex_matrix(vec);
 
     let svd = mat.svd(true, true);
 
-    let ellipse = ell::ellipse(20.0, 30.0, 10.0, 4.1);
+    for i in &ell_vec {
+        for j in &ell_vec {
+            for k in &ell_vec {
+                for l in &perm_vec {
+                    let ellipse = ell::ellipse(*i, *j, *k, *l as f32);
+                    tensor_a1.push(ellipse.0);
+                    tensor_a2.push(ellipse.1);
+                    tensor_a3.push(ellipse.2);
+      //              write!(file, "{:?}, {:?}, {:?},  ", ellipse.0, ellipse.1, ellipse.2)?;
+                    //println!("Pusing tensor updates");
+                }
+            }
+        }
+    }
 
+    //    println!("Right-Singular Vector {:.3?}", svd.v_t);
+    //    println!("Left-Singular Vector {:.3?}", svd.u);
+   
+
+    let singular_values_1 = svd.singular_values[(0, 0)];
+    let singular_values_2 = svd.singular_values[(1, 0)];
+    let singular_values_3 = svd.singular_values[(2, 0)];
+
+    let mut tensor_index_a1: Vec<usize> = Vec::new();
+    let mut tensor_index_a2: Vec<usize> = Vec::new();
+    let mut tensor_index_a3: Vec<usize> = Vec::new();
+
+    for (count, v) in tensor_a1.iter().enumerate() {
+        if v <= &(singular_values_1 + 5.0) && v >= &(singular_values_1 - 5.0) {
+            tensor_index_a1.push(count);
+           // println!("COUNT: {:?}, V: {:?}", count, v);
+        }
+    }
+    println!("Found {:?} matches for a1", tensor_index_a1.len());
+    //println!("a1 {:?}, a2 {:?}, a3, {:?} ", tensor_a1[tensor_index_a1]
+    for (_count, v) in tensor_index_a1.iter().enumerate(){
+	if (tensor_a2[*v] <= singular_values_2 + 5.0) && tensor_a2[*v] >= (singular_values_2 - 5.0){
+	    tensor_index_a2.push(*v);
+	   // println!("found a2: {:?}", v);
+	}
+    }
+     println!("Found {:?} matches for a2", tensor_index_a2.len());
+
+    for (_count, v) in tensor_index_a2.iter().enumerate(){
+	if (tensor_a3[*v] <= singular_values_3 + 5.0) && tensor_a3[*v] >= (singular_values_3 - 5.0){
+	    tensor_index_a3.push(*v);
+	    //println!("found a3: {:?}", v);
+	}
+    }
+    println!("Found {:?} matches for a3", tensor_index_a3.len());
+
+    for (_count, v) in tensor_index_a3.iter().enumerate(){
+	println!("========================================");
+	println!("Index is -- {:?}", v);
+	
+	println!("a1 ---- {:?}", tensor_a1[*v]);
+	println!("a2 ---- {:?}", tensor_a2[*v]);
+	println!("a3 ---- {:?}", tensor_a3[*v]);
+       
+	println!("Singular Values {:.3?}", svd.singular_values);
+	println!("========================================");
+    } 
     
-    
-    println!("Right-Singular Vector {:.3?}", svd.v_t);
-    println!("Left-Singular Vector {:.3?}", svd.u);
-    println!("Singular Values {:.3?}", svd.singular_values);
-    println!("{:?}", ellipse);
-    
+    Ok(())
 }
