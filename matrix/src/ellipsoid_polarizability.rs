148094ee7620fe86d1a63429e70196669478066f
TJ <tyler.jowaisas@gmail.com>
2022-11-16 21:57:35 -0500

    adding rayon.  seems to be helping.  need to finish making more of the loops into iterators so they're easy to parallize

--- matrix/src/ellipsoid_polarizability.rs
+++ matrix/src/ellipsoid_polarizability.rs
@@ -1,8 +1,11 @@
 //used to find the best fit model of an ellipsoid
 use std::f32::consts::PI;
+use rayon::prelude::*;
 
 pub fn ellipse(a1: f32, a2: f32, a3: f32, mu: f32) -> (f32, f32, f32, f32, f32, f32, f32, f32) {
     let volume = (4.0 / 3.0) * PI * a1 * a2 * a3;
+
+    let loop_number = 100000.0;
 
     let normal = volume.cbrt();
 
@@ -10,47 +13,41 @@ pub fn ellipse(a1: f32, a2: f32, a3: f32, mu: f32) -> (f32, f32, f32, f32, f32, 
     let a2 = a2 / normal;
     let a3 = a3 / normal;
 
-    let Xm = mu - 1.0;
-    //let normalize = (volume.cbrt()).powf(2.0);
-
-    //let semi_axis = vec![a1, a2, a3];
-    //let mut normal_axis: Vec<f32> = Vec::new();
-
-    // for i in &mut normal_axis {
-    //    *i *= normalize;
-    // }
-    /*
-    println!("a1  to: {:.3?}", a1);
-    println!("a2  to: {:.3?}", a2);
-    println!("a3  to: {:.3?}", a3);
-    */
 
     let ds = 0.1;
-    let mut n1 = 0.0;
-    let mut n2 = 0.0;
-    let mut n3 = 0.0;
+    let mut n1: Vec<f32> = Vec::new();
+    let mut n2: Vec<f32> = Vec::new();
+    let mut n3: Vec<f32> = Vec::new();
 
-    let tensor_index_a1: Vec<usize> = Vec::new();
-    let mut tensor_index_a2: Vec<usize> = Vec::new();
-    let mut tensor_index_a3: Vec<usize> = Vec::new();
 
-    for x in 1..1000000 {
-        let mut s = x as f32 * ds;
-        s = s - ds / 2.0;
+    let pow_a1 = a1.powf(2.0);
+    let pow_a2 = a2.powf(2.0);
+    let pow_a3 = a3.powf(2.0);
 
-        n1 += ds
-            / ((s + a1.powf(2.0))
-                * ((s + a1.powf(2.0)) * (s + a2.powf(2.0)) * (s + a3.powf(2.0))).sqrt());
-        n2 += ds
-            / ((s + a2.powf(2.0))
-                * ((s + a1.powf(2.0)) * (s + a2.powf(2.0)) * (s + a3.powf(2.0))).sqrt());
-        n3 += ds
-            / ((s + a3.powf(2.0))
-                * ((s + a1.powf(2.0)) * (s + a2.powf(2.0)) * (s + a3.powf(2.0))).sqrt());
-    }
-    n1 = n1 * (a1 * a2 * a3 / 2.0);
-    n2 = n2 * (a1 * a2 * a3 / 2.0);
-    n3 = n3 * (a1 * a2 * a3 / 2.0);
+    let xm = mu - 1.0;
+
+    let dump = vec![0.0, loop_number];
+    let index:Vec<f32> = dump.par_iter().map(|x| x + ds).collect();
+
+    
+    
+    let denominator = index.par_iter().map(|d|
+        ((d + pow_a1) * (d + pow_a2) * (d + pow_a3)).sqrt()).collect();
+   
+
+        //need to finish making all of these as Rust iterators so they're easy to parallize
+    n1.push( ds / ((s + pow_a1) * denominator));
+    n2.push( ds / (( s+ pow_a2) * denominator));
+    n3.push( ds / (( +s pow_a3) * denominator));
+   
+    
+    let n1: f32 = n1.par_iter().sum();
+    let n2: f32 = n2.par_iter().sum();
+    let n3: f32 = n3.par_iter().sum();
+
+    let n1 = n1 * (a1 * a2 * a3 / 2.0);
+    let n2 = n2 * (a1 * a2 * a3 / 2.0);
+    let n3 = n3 * (a1 * a2 * a3 / 2.0);
 
     //println!("n1  to: {:.3?}", n1*10000000.00);
     //println!("n2  to: {:.3?}", n2);
@@ -58,9 +55,9 @@ pub fn ellipse(a1: f32, a2: f32, a3: f32, mu: f32) -> (f32, f32, f32, f32, f32, 
 
     // println!("Volume is: {:.3?}", volume);
 
-    let alpha1: f32 = volume * Xm / (1.0 + Xm * n1);
-    let alpha2: f32 = volume * Xm / (1.0 + Xm * n2);
-    let alpha3: f32 = volume * Xm / (1.0 + Xm * n3);
+    let alpha1: f32 = volume * xm / (1.0 + xm * n1);
+    let alpha2: f32 = volume * xm / (1.0 + xm * n2);
+    let alpha3: f32 = volume * xm / (1.0 + xm * n3);
 
     return (alpha1, alpha2, alpha3, volume, a1, a2, a3, mu);
 }
